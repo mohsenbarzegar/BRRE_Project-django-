633eef9e61415aea19e2ae711fafe7c5f3209a11
mohsen <mohsenbarzegar94@gmail.com>
2019-06-09 11:05:43 +0430

    listings app completed.

--- listings/views.py
+++ listings/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from .models import Listing
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
-    return render(request,'listings/listings.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    contex = {
+        'listings':paged_listings
+    }
+    return render(request,'listings/listings.html', contex)
 
-def listing(request):
+def listing(request, listing_id):
     return render(request,'listings/listing.html')
 
 def search(request):
